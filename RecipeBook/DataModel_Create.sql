b3d1f90f43348f0b2fc2ee6dd47cbf6b9b776c2a
Thaddeus R. Winker <trwinker@gmail.com>
2015-12-06 16:24:11 -0500

    WPF Application and database model

---
+++ RecipeBook/DataModel_Create.sql
@@ -0,0 +1,27 @@
+﻿create table Measurements(
+	mes_ID int identity primary key
+	, mes_Name varchar(30)
+	, mes_Description varchar(200)
+	, mes_Type int
+	)
+create table Ingredients(
+	ing_ID int identity primary key
+	, ing_Name varchar(30)
+	, ing_Description varchar(200)
+	)
+create table Recipes (
+	rec_ID int identity primary key
+	, rec_Name varchar(100)
+	, rec_Description varchar(200)
+	, rec_Source varchar(60)
+	, rec_PreparationInstructions varchar(500)
+	, rec_CookingInstructions varchar(500)
+	, rec_EntryDate date
+	)
+create table RecipeIngredients(
+	ri_ID int identity primary key
+	, rec_ID int foreign key references Recipes(rec_ID)
+	, ing_ID int foreign key references Ingredients(ing_ID)
+	, mes_ID int foreign key references Measurements(mes_ID)
+	, ri_Amount float
+	)
---
+++ RecipeBook/RecipeComponents.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeBook.Components
+{
+    class RecipeComponents
+    {
+
+    }
+    public enum MeasurementTypes
+    {
+        _default = 0,
+        weight,
+        volume,
+        count
+    }
+}
