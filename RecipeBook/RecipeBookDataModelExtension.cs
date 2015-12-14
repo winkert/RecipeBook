d84451a6ed54773e23efdb3f90c4647ae926a0da
Thaddeus R. Winker <trwinker@gmail.com>
2015-12-14 11:07:39 -0500

    extension of data model. Resolving issue from user lovebdsobuj

---
+++ RecipeBook/RecipeBookDataModelExtension.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+
+namespace RecipeBook
+{
+    public partial class Ingredient : INotifyPropertyChanging, INotifyPropertyChanged
+    {
+        public override string ToString()
+        {
+            return ing_Name;
+        }
+    }
+    public partial class Measurement : INotifyPropertyChanging, INotifyPropertyChanged
+    {
+        public override string ToString()
+        {
+            return mes_Name;
+        }
+    }
+    public partial class Recipe : INotifyPropertyChanging, INotifyPropertyChanged
+    {
+        public override string ToString()
+        {
+            return rec_Name;
+        }
+    }
+    public partial class RecipeCategory : INotifyPropertyChanging, INotifyPropertyChanged
+    {
+        public override string ToString()
+        {
+            return cat_Name;
+        }
+    }
+}
